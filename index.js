e2657702b19a120350df5557dbd7fdc1cb2d5ab6
Dina <dinagiacobbi@gmail.com>
2023-07-09 13:59:25 -0800

    Adding json response from article sources

--- index.js
+++ index.js
@@ -6,6 +6,33 @@ const cheerio = require('cheerio')
 
 const app = express()
 
-app.listen(PORT, ()=> {
-    console.log(`Server Running on ${PORT}`)
+const articles = []
+
+app.get('/', (req, res) => {
+  res.json('Welcome to Climate Change News API')
+})
+
+app.get('/news', (req, res) => {
+  axios
+    .get('https://www.theguardian.com/environment/climate-crisis')
+    .then((response) => {
+      const html = response.data
+      const totalInfo = cheerio.load(html)
+      totalInfo('a:contains("climate")', html).each(function () {
+        const title = totalInfo(this).text()
+        const url = totalInfo(this).attr('href')
+        articles.push({
+          title,
+          url,
+        })
+      })
+      res.json(articles)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server Running on ${PORT}`)
 })
